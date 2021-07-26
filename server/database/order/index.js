8568dc4ea7928934372235f1231601998130221c
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-07-26 19:06:24 +0530

    add all schema

---
+++ server/database/order/index.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+    {
+        user:{
+            type:mongoose.Types.ObjectId,
+            ref:"Users",
+        },
+        orderDetails:[
+            {
+                food:{type:mongoose.Types.ObjectId, ref:"Foods"},
+                quantity:{type:Number,required:true},
+                paymode:{type:String,required:true},
+                status:{type:String, default:"Placed"},
+                paymentDetails:{
+                    itemTotal:{type:Number,required:true},
+                    promo:{type:Number,required:true}, //stores amount deducted after promo code applied
+                    tax:{type:Number,required:true},
+                },
+            },
+        ],
+        orderRatings:{
+            type:Number,
+            required:true,
+        },
+    }
+);
+
+export const OrderMOdel = mongoose.model("Orders", OrderSchema );
