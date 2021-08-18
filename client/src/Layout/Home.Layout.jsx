a5f6222f63e5ca831d12bd09ac4d665ecce40df6
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-08-18 19:53:39 +0530

    add foodtab

--- client/src/Layout/Home.Layout.jsx
+++ client/src/Layout/Home.Layout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+
 //components
 import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
 
 const HomeLayout = (props)=>{
     return (
@@ -10,7 +12,7 @@ const HomeLayout = (props)=>{
        <div className="container mx-auto px-4 lg:px-20">
         {props.children}
        </div>
-       
+       <FoodTab /> 
     </>
     );
 };
